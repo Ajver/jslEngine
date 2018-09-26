c760f7f9368969cb01c330d042af3f1d8dc2aa64
Ajver <zawlockidominik@gmail.com>
2018-09-26 17:46:56 +0200

    ...

--- src/MainFiles/MainClass.java
+++ src/MainFiles/MainClass.java
@@ -21,23 +21,21 @@ public class MainClass extends jslEngine {
         jsl.onHoverButtonSettings = onHoverSettings;
 
         jslButton btn = jsl.newButton("HI", 100, 100, 200, 50);
-
-        System.out.println("Button xy wh: " + btn.getX());
     }
 
     protected void update(float et) {
-        for(int i=0; i<100000; i++) {
-            for(int j=0; j<1; j++) {
-                new Random().nextInt(10000);
-            }
-        }
+//        for(int i=0; i<100000; i++) {
+//            for(int j=0; j<1; j++) {
+//                new Random().nextInt(10000);
+//            }
+//        }
     }
 
     protected void render(Graphics g) {
         g.setColor(Color.WHITE);
         g.drawString("FPS: "+getFpsCount(), 50, 50);
-        g.drawString("WW: "+WW(), 50, 100);
-        g.drawString("WH: "+WH(), 50, 125);
+//        g.drawString("WW: "+WW(), 50, 100);
+//        g.drawString("WH: "+WH(), 50, 125);
     }
 
     public static void main(String[] args) {
