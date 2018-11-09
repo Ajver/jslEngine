8e772d2fa5fce9551589be336ab63d6220edd529
Ajver <zawlockidominik@gmail.com>
2018-11-09 16:19:00 +0100

    Add example Rect class

--- src/MainFiles/Rect.java
+++ src/MainFiles/Rect.java
@@ -2,11 +2,17 @@ package MainFiles;
 
 import jslEngine.jslObject;
 
+import java.awt.*;
+
 public class Rect extends jslObject {
 
-    public Rect() {
-
+    public Rect(float x, float y, float w, float h) {
+        super(x, y, w, h);
     }
 
+    public void render(Graphics g) {
+        g.setColor(new Color(255, 255, 0));
+        g.fillRect((int)x, (int)y, (int)w, (int)h);
+    }
 
 }
